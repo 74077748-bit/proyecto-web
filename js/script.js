6b6ae390e8ffe2e65ca364d4a0a0a7fe0c3bdb76
74077748-bit <74077748@idexpasco.edu.pe>
2026-04-29 15:43:46 -0500

    Actualizar páginas y corregir estilos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-document.addEventListener("DOMContentLoaded",() =>  {
+document.addEventListener("DOMContentLoaded", () => {
     console.log("Sitio cargando correctamente");
 
-
     const btnTop = document.getElementById("btnTop");
-    if (btnTop){
-        window.addEventListener("scroll", () =>{
-            btnTop.style.display = window.scrollY >200? "block" : "none"; 
+    if (btnTop) {
+        window.addEventListener("scroll", () => {
+            btnTop.style.display = window.scrollY > 200 ? "block" : "none";
         });
-        btnTop.addEventListener("click" , () => {
-            window.scrollTo({ top: 0, behavior: "smooth"});
+        btnTop.addEventListener("click", () => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
         });
-    };
+    }
+
+    // Agregar un borde rojo a todo el contenido
+    document.body.style.border = "5px solid red";
+
+    // Hora al hacer clic - Sencillo y bonito
+document.body.addEventListener("click", () => {
+    alert("🕐 " + new Date().toLocaleTimeString());
+});
 });
